cb3f0917d02c3507fc42de03853d792bdab56858
ligain <khokhlov86@gmail.com>
2019-10-10 23:09:34 +0300

    reconnect feature

--- read_minechat.py
+++ read_minechat.py
@@ -1,24 +1,48 @@
 import asyncio
 import logging
+import socket
+from concurrent.futures import TimeoutError
 
 
-async def read_chat(host='minechat.dvmn.org', port=5001):
-    reader, writer = await asyncio.open_connection(host, port)
+async def connect(host='minechat.dvmn.org', port=5000, attempts=10, timeout=2):
+    while attempts:
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+            return reader, writer
+        except socket.gaierror:
+            print(f'Cannot connect to {host}:{port}. Next retry in {timeout} seconds. {attempts} attempts lost')
+            attempts -= 1
+            timeout += 2
+            await asyncio.sleep(timeout)
+    print(f'Abort connecting to {host}:{port}')
+    raise asyncio.CancelledError
 
-    print(f'Start listening on: {host}:{port}')
+
+async def read_chat(host='minechat.dvmn.org', port=5000):
+    reader, writer = await connect()
+    print(f'Start listening to {host}:{port}')
 
     try:
-        while True:
-            line = await reader.readline()
-            if not line:
-                break
-            line = line.decode().rstrip()
-            print(f'Received: {line}')
+        while not reader.at_eof():
+            try:
+                line = await asyncio.wait_for(reader.readline(), 11)
+                line = line.decode().rstrip()
+                print(f'Received: {line}')
+            except TimeoutError:
+                print('No connection. Try to reconnect...')
+                writer.close()
+                reader, writer = await connect()
+
     finally:
         print('Close the connection...')
         writer.close()
 
-try:
-    asyncio.run(read_chat())
-except KeyboardInterrupt:
-    print('Interrupted by user.')
+
+if __name__ == '__main__':
+    # logging.getLogger("asyncio").setLevel(logging.DEBUG)
+    try:
+        asyncio.run(read_chat())
+    except KeyboardInterrupt:
+        print('Interrupted by user.')
+    except asyncio.CancelledError:
+        print('Bye!')
